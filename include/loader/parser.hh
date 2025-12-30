4d3e565177aa05ce1100483af6f4ae67e63c0b88
Ryan Nelson <ryannelson6@hotmail.com>
2025-12-29 19:17:05 -0800

    Parser finished

--- include/loader/parser.hh
+++ include/loader/parser.hh
@@ -1,12 +1,21 @@
 #pragma once
 
+// character used to separate keywords
+#define KEYWORD_SEPARATOR ':'
+
 #include <string>
 #include <unordered_map>
 #include <iostream>
+#include <stdexcept>
 
 using std::unordered_map;
 using std::string;
 using std::istream;
+
+class parse_error : public std::runtime_error { 
+	public:
+	parse_error(string msg) : std::runtime_error(msg) {}
+};
 
 struct node {
 	node(string value) : value(value), nxt(nullptr) { }
@@ -26,5 +35,6 @@ struct node {
 
 /**
  * Parses a file into the key-values contained within
+ * Nodes in collection must be freed
  */
-unordered_map<string, node> parsefile(istream file);
+unordered_map<string, node*> parsefile(istream file);
--- loader/parser.cc
+++ loader/parser.cc
@@ -1,5 +1,29 @@
 #include "parser.hh"
 
-unordered_map<string, node> parsefile(istream file) {
-	// TODO
+unordered_map<string, node*> parsefile(istream file) {
+	unordered_map<string, node*> rvalue;
+	while (!file.eof()) {
+		string line;
+		file >> line;
+		size_t lag = line.find_first_of(KEYWORD_SEPARATOR);
+		if (lag == line.npos) throw new parse_error("invalid line (no separator): " + line);
+		string key(line, 0, lag);
+		node* nd = nullptr;	
+	
+		bool loop = true;
+		while (loop) {
+			size_t fnt = line.find_first_of(KEYWORD_SEPARATOR, lag + 1);
+			if (fnt == line.npos) {
+				loop = false;
+				fnt = line.length();
+			}
+			string value(line, lag + 1, fnt - lag - 1);
+			if (value.empty()) throw new parse_error("Empty value for key: " + key);
+			nd = new node(value, nd);
+			lag = fnt;
+		}
+
+		rvalue[key] = nd;
+	}
+	return rvalue;
 }
