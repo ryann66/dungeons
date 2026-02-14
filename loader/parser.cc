9e3588c05fd85384de8e0a822106fd82ea7cd7d8
Ryan Nelson <ryannelson6@hotmail.com>
2026-02-13 21:27:58 -0800

    Bugfix: parser memory leak with duplicate keys

--- loader/parser.cc
+++ loader/parser.cc
@@ -4,32 +4,45 @@ namespace loader {
 
 unordered_map<string, node*> parsefile(istream file) {
 	unordered_map<string, node*> rvalue;
-	while (!file.eof()) {
-		string line;
-		file >> line;
-		size_t lag = line.find_first_of(KEYWORD_SEPARATOR);
-		if (lag == line.npos)
-			throw new parse_error("invalid line (no separator): " + line);
-		string key(line, 0, lag);
-		node* nd = nullptr;
+	try {
+		while (!file.eof()) {
+			string line;
+			file >> line;
+			size_t lag = line.find_first_of(KEYWORD_SEPARATOR);
+			if (lag == line.npos)
+				throw new parse_error("invalid line (no separator): " + line);
+			string key(line, 0, lag);
+			node* nd = nullptr;
 
-		bool loop = true;
-		while (loop) {
-			size_t fnt = line.find_first_of(KEYWORD_SEPARATOR, lag + 1);
-			if (fnt == line.npos) {
-				loop = false;
-				fnt = line.length();
+			bool loop = true;
+			while (loop) {
+				size_t fnt = line.find_first_of(KEYWORD_SEPARATOR, lag + 1);
+				if (fnt == line.npos) {
+					loop = false;
+					fnt = line.length();
+				}
+				string value(line, lag + 1, fnt - lag - 1);
+				if (value.empty()) {
+					delete nd;
+					throw new parse_error("Empty value for key: " + key);
+				}
+				nd = new node(value, nd);
+				lag = fnt;
 			}
-			string value(line, lag + 1, fnt - lag - 1);
-			if (value.empty())
-				throw new parse_error("Empty value for key: " + key);
-			nd = new node(value, nd);
-			lag = fnt;
-		}
 
-		rvalue[key] = nd;
+			if (!rvalue.insert({key, nd}).second) {
+				delete nd;
+				throw new parse_error("Duplicate definitions for key: " + key);
+			}
+		}
+		return rvalue;
+	} catch (parse_error* error) {
+		for (auto itr = rvalue.begin(); itr != rvalue.end();) {
+			delete itr->second;
+			itr = rvalue.erase(itr);
+		}
+		throw error;
 	}
-	return rvalue;
 }
 
 } // namespace loader
